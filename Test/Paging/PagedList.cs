82acf88e202144e270f107aab22f73aadc4d3765
Minh Le Cong <minh.le@tpssoft.com>
2022-12-19 09:54:41 +0700

    fix api

--- Test/Paging/PagedList.cs
+++ Test/Paging/PagedList.cs
@@ -4,21 +4,7 @@
     {
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
-        //public int PageSize { get; set; }
-        //public int TotalCount { get; set; }
         public List<T> data { get; set; } = new();
-        //public bool HasPrevious => CurrentPage > 1;
-        //public bool HasNext => CurrentPage < TotalPages;
-        //public PagedList(List<T> items, int count, int pageNumber, int pageSize)
-        //{
-        //    TotalCount = count;
-        //    PageSize = pageSize;
-        //    CurrentPage = pageNumber;
-        //    TotalPages = (int)Math.Ceiling(count / (double)PageSize);
-        //    AddRange(items); 
-
-           
-        //}
 
         public PagedList(int PageNumber, int TotalPages, List<T>items)
         {
@@ -27,11 +13,5 @@
             this.data = items;
         }
 
-        //public static PagedList<T> GetPagedList(IQueryable<T> source, int pageNumber, int pageSize)
-        //{
-        //    var count = source.Count();
-        //    var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-        //    return new PagedList<T>(items, count, pageNumber, pageSize);
-        //}
     }
 }
